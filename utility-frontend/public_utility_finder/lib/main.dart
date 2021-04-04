88a03bdea8e6a648f295788469264955a59a195b
YANG, BRIAN <s-BRYANG@lwsd.org>
2021-04-03 20:08:29 -0400

    test

--- utility-frontend/public_utility_finder/lib/main.dart
+++ utility-frontend/public_utility_finder/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,8 +12,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
+  late Position currentPosition;
+  String currentAddress = "";
 
+  final Geolocator locator = Geolocator() .. forceAndroidLocationManager;
   final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  getCurrentLocation()
+  {
+    locator.getCurrentPosition(desiredAccuracy:LocationAccuracy.best).then((Position pos)
+    {
+      setState(() 
+      {
+        currentPosition = pos;
+      });
+      getAddress();
+    }).catchError((err)
+    {
+      print(err);
+    });
+  }
+
+getAddress() async
+{
+  try
+  {
+    List<Placemark> places = await 
+    locator.placemarkFromCoordinates(currentPosition.latitude, 
+    currentPosition.longitude);
+
+    Placemark p = places[0];
+    setState(()
+    {
+      currentAddress = "${p.locality}, ${p.postalCode}, ${p.country}";
+    });
+  }
+  catch (excep)
+  {
+    print(excep);
+  }
+}
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
